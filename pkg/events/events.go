7902c4c16ae837b9a6975f3a7ea91791c884686a
Jeff Lindsay <progrium@gmail.com>
2016-11-11 17:00:59 -0600

    pkg/events: docs and examples
    Signed-off-by: Jeff Lindsay <progrium@gmail.com>

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -4,36 +4,58 @@ import "sync"
 
 var defaultEmitter = &Emitter{}
 
+// Event interface is used to identify events
 type Event interface {
 	EventName() string
 }
 
+// Signal is a builtin event that is just a name with no other payload
+type Signal string
+
+func (s Signal) EventName() string {
+	return string(s)
+}
+
+// Listener wraps an event handler callback, optionally specifying how
+// it receives events from an Emitter
 type Listener struct {
+	// If not empty, only receive events with this name
 	EventName string
-	Once      bool
-	Filter    func(Event) bool
-	Handler   func(Event)
+
+	// If true, will fire only once and then listener will be removed
+	Once bool
+
+	// If not nil, a callback to determine if handler will be called
+	Filter func(Event) bool
+
+	// Callback to handle event
+	Handler func(Event)
 }
 
+// Emitter is a collection of Listeners that you can Emit events to
 type Emitter struct {
-	sync.Mutex
 	listeners []*Listener
+	mu        sync.Mutex
 }
 
+// Listen adds a listener to receive events on the default emitter.
+//
+// If Handler is nil, the listener is ignored.
 func Listen(listener *Listener) { defaultEmitter.Listen(listener) }
 func (e *Emitter) Listen(listener *Listener) {
 	if listener.Handler == nil {
 		return
 	}
-	e.Lock()
-	defer e.Unlock()
+	e.mu.Lock()
+	defer e.mu.Unlock()
 	e.listeners = append(e.listeners, listener)
 }
 
+// Unlisten removes a listener from receiving events on the default emitter.
 func Unlisten(listener *Listener) { defaultEmitter.Unlisten(listener) }
 func (e *Emitter) Unlisten(listener *Listener) {
-	e.Lock()
-	defer e.Unlock()
+	e.mu.Lock()
+	defer e.mu.Unlock()
 	var i int
 	var l *Listener
 	var found bool
@@ -48,10 +70,15 @@ func (e *Emitter) Unlisten(listener *Listener) {
 	}
 }
 
+// Emit triggers the handlers of listeners that match the event, passing
+// the event to them.
+//
+// If it triggers a listener with Once set to true, the listener will be removed
+// after triggering.
 func Emit(event Event) { defaultEmitter.Emit(event) }
 func (e *Emitter) Emit(event Event) {
-	e.Lock()
-	defer e.Unlock()
+	e.mu.Lock()
+	defer e.mu.Unlock()
 	eventName := event.EventName()
 	var keep []*Listener
 	for _, listener := range e.listeners {
